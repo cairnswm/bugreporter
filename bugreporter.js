153529a9a62bcd68800683098adf25c6327dbb9b
William Cairns <cairnswm@gmail.com>
2020-07-31 14:48:13 +0200

    Refactor: for Bootstrap css (vanilla JS)

--- bugreporter.js
+++ bugreporter.js
@@ -20,31 +20,14 @@ function getWidth() {
   }
 
 // ===================
-// BUGREPORTER
+// Header: BUGREPORTER
 // ===================
-// TODO: Make into an object that a user can reference - eg. bugReporter.<method>
-// TODO: Post image to server
-// TODO: Add object config (url, classes etc)
-// TODO: Add popup menu on right click, not immediatly create a but
 
 // SET UP BUGREPORTER ON PAGE RIGHT CLICK
-// TODO: Move login into Class
 window.addEventListener("contextmenu", 
     function(e){
-        console.log(e.pageX,' - ',e.pageY);
-        elementMouseIsOver = document.elementFromPoint(e.offsetX, e.offsetY);
-        console.log("PointXY",elementMouseIsOver);
         console.log(e);
-        e.stopPropagation();
-        e.preventDefault();
-        let scr = BugReporter.getScreenShot(elementMouseIsOver);
-        scr.then(c => {
-            console.log("THEN");
-            console.log(scr,c);
-            document.getElementById("bugreport-image").innerHTML = "";
-            document.getElementById("bugreport-image").appendChild(c);
-        });
-        BugReporter.openPopup("bugreport");
+        BugReporter.execute(e);
         return false;
         // https://stackoverflow.com/questions/13198131/how-to-save-an-html5-canvas-as-an-image-on-a-server
     }, false);
@@ -53,39 +36,99 @@ const BugReporterClass = {
     version: function()  { return "0.0.1" },
 
     init: function() {
-        this.createStyle();
         this.createhtml();
     }, 
 
+    execute(e) {
+        console.log(e.pageX,' - ',e.pageY);
+        elementMouseIsOver = document.elementFromPoint(e.offsetX, e.offsetY);
+        console.log("PointXY",elementMouseIsOver);
+        console.log(e);
+        e.stopPropagation();
+        e.preventDefault();
+        let scr = this.getScreenShot(elementMouseIsOver);
+        scr.then(c => {
+            console.log("THEN");
+            console.log(scr,c);
+            document.getElementById("bugreport-image").innerHTML = "";
+            document.getElementById("bugreport-image").appendChild(c);
+            this.openPopup("bugreport");
+        });
+        
+    },
+
     createhtml() {
         // INJECT BUGREPORTER HTML
-        document.body.innerHTML = document.body.innerHTML + `
-            <div class="body-blackout"></div>
-            <div class="bugreportermodal shadow" data-bugreportermodal="bugreport"> 
-                <span class="text-white bg-primary bugreportermodal__close" style="right:10px;top:10px">X</span>
-                <div class="font-weight-bold bugr-head">Log a Bug Report</div>
-                <div id="debugContent">
-                    Please describe the error you have identified<br/>
-                    <input type="text"></input>
-                    <button>Save</button>
+         document.body.innerHTML = document.body.innerHTML + `
+             <div class="body-blackout hide"></div>
+        `;
+        //     <div class="bugreportermodal hide shadow" data-bugreportermodal="bugreport"> 
+        //         <span class="text-white bg-primary bugreportermodal__close" style="right:10px;top:10px">X</span>
+        //         <span aria-hidden="true">&times;</span>
+        //         <div class="font-weight-bold bugr-head">Log a Bug Report</div>
+        //         <div id="debugContent">
+        //             <div class="input-group mb-3">
+        //                 <input type="text" class="form-control" placeholder="Description" aria-label="Description of Bug" aria-describedby="button-addon2">
+        //                 <div class="input-group-append">
+        //                 <button class="btn btn-primary" type="button" id="button-addon2">Button</button>
+        //             </div>
+        //         </div>
+        //         </div>
+        //         <div class="debugImageContent">
+        //             <div class="bugreport-image" id="bugreport-image"></image>
+        //         </div>
+        //     </div>
+        //     `;
+            document.body.innerHTML = document.body.innerHTML + `            
+            <div class="modal fade bugreportermodal" id="bugreport" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true" data-bugreportermodal="bugreport">
+            <div class="modal-dialog">
+              <div class="modal-content">
+                <div class="modal-header">
+                  <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                  <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span id="bugreportermodal__close" aria-hidden="true">&times;</span>
+                  </button>
                 </div>
-                <div class="bugreport-image" id="bugreport-image"></image>
+                <div class="modal-body">
+                    <div class="input-group mb-3">
+                        <input type="text" class="form-control" placeholder="Description" aria-label="Description of Bug" aria-describedby="button-addon2">
+                        <div class="input-group-append">
+                        <button class="bugreportermodal__close btn btn-primary" type="button" id="bugreporter__save1">Save</button>
+                        </div>
+                    </div>
+                    <div class="bugreport-image" id="bugreport-image"></image></div>
+                </div>
+                <div class="modal-footer">
+                  <button id="bugreporter__save2" type="button" class="btn btn-primary" onClick="() => BugReporter.closePopup()">Save changes</button>
+                </div>
+              </div>
             </div>
-            `;
+          </div>`;
         // GET BUGREPORTER BACKGROUND 
-        this.bodyBlackout = document.querySelector('.body-blackout');
+        this.bodyBlackout = document.querySelector('.body-blackout');        
         // CREATE CLOSE POPUP EVENTS
-        document.querySelector('.bugreportermodal__close').addEventListener('click', (event) => {
-            popupModal = event.target.closest(".bugreportermodal");
-                console.log("close");
-                popupModal.classList.remove('is--visible');
-                this.bodyBlackout.classList.remove('is-blacked-out');
-            })
+        document.querySelector('#bugreportermodal__close').addEventListener('click', (event) => {
+            BugReporter.closePopup();
+        })
+        document.querySelector('#bugreporter__save1').addEventListener('click', (event) => {
+            BugReporter.closePopup();
+        })
+        document.querySelector('#bugreporter__save2').addEventListener('click', (event) => {
+            BugReporter.closePopup();
+        })
     },
     openPopup(popupId) {
-        const popupModal = document.querySelector(`[data-bugreportermodal="${popupId}"]`);
+        const popupModal = document.querySelector(`[data-bugreportermodal="bugreport"]`);
         popupModal.classList.add('is--visible');
         this.bodyBlackout.classList.add('is-blacked-out');
+        
+        popupModal.classList.remove('fade');
+    },
+    closePopup(popupId) {
+        const popupModal = document.querySelector(`[data-bugreportermodal="bugreport"]`);
+        popupModal.classList.remove('is--visible');
+        this.bodyBlackout.classList.remove('is-blacked-out');        
+        popupModal.classList.add('fade');
     },
 
     // CREATE 50% SCREEN SHOT
@@ -118,73 +161,6 @@ const BugReporterClass = {
             }
         });
         return promise;            
-    },
-
-    createStyle() {
-        // ADD STYLES TO DOCUMENT
-        var styles = `
-        .body-blackout {
-            position: absolute;
-            z-index: 1010;
-            left: 0;
-            top: 0;
-            width: 100%;
-            height: 100%;
-            background-color: rgba(0, 0, 0, .65);
-            display: none;
-            overflow: hidden;
-        }
-        
-        .body-blackout.is-blacked-out {
-            display: block;
-        }
-
-        .popup-trigger {
-        display: inline-block;
-        }
-
-        .bugreportermodal {
-            height: 365px;
-            width: 60%;
-            background-color: #fff;
-            position: absolute;
-            left: 50%;
-            top: 50%;
-            transform: translate(-50%, -50%);
-            padding: 10px;
-            opacity: 0;
-            pointer-events: none;
-            transition: all 300ms ease-in-out;
-            z-index: 1011;
-            border-radius: 5px;
-        }
-        .bugreportermodal.is--visible {
-            opacity: 1;
-            pointer-events: auto;
-        }
-        
-        .bugreportermodal__close {
-            position: absolute;
-            font-size: 1.2rem;
-            right: -10px;
-            top: -10px;
-            cursor: pointer;
-        }
-        
-        .bugr-head {
-            font-size:large;
-        }
-        
-        .bugreport-image {  
-            height: 70%;      
-            overflow:auto;
-            position:absolute;
-        }`;
-        var styleSheet = document.createElement("style");
-        styleSheet.type = "text/css";
-        styleSheet.innerText = styles;
-        document.head.appendChild(styleSheet);
-        console.log("StyleSheet");
     }
 }    
 
---
+++ todolist.md
@@ -0,0 +1,5 @@
+// TODO: 1: Make into an object that a user can reference - eg. bugReporter.<method>
+// TODO: 2: Post image to server
+// TODO: 3: Add object config (url, classes etc)
+// TODO: 5: Add popup menu on right click, not immediatly create a but
+// TODO: 6: Move context menu event into Class

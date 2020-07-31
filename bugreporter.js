8ce6b8d9adbe54fd19009f9bb73da08096460f77
William Cairns <cairnswm@gmail.com>
2020-07-31 09:04:01 +0200

    Feature: Display bugreporter modal

--- bugreporter.js
+++ bugreporter.js
@@ -1,4 +1,25 @@
+function getWidth() {
+    return Math.max(
+      document.body.scrollWidth,
+      document.documentElement.scrollWidth,
+      document.body.offsetWidth,
+      document.documentElement.offsetWidth,
+      document.documentElement.clientWidth
+    );
+  }
+  
+  function getHeight() {
+    return Math.max(
+      document.body.scrollHeight,
+      document.documentElement.scrollHeight,
+      document.body.offsetHeight,
+      document.documentElement.offsetHeight,
+      document.documentElement.clientHeight
+    );
+  }
+
 // CREATE 50% SCREEN SHOT
+
 async function getScreenShot(elementMouseIsOver) {
     var promise = new Promise(function(resolve, reject) {
         try {
@@ -7,8 +28,10 @@ async function getScreenShot(elementMouseIsOver) {
                 console.log(canvas);
                 var c = document.createElement("canvas");
                 var ctx = c.getContext("2d");
-                ctx.width=400;
-                ctx.height=400;
+                c.width=(getWidth()+50)/2;
+                c.height=(getHeight()+50)/2;
+                ctx.width=1000; //getWidth()+50;
+                ctx.height=getHeight()+50;
                 ctx.scale(0.5,0.5);
                 ctx.drawImage(canvas,0,0);
                 // HIGHLIGHT CLICKED ELEMENT
@@ -16,7 +39,7 @@ async function getScreenShot(elementMouseIsOver) {
                 ctx.lineWidth = 10;
                 var clientRectangle = elementMouseIsOver.getBoundingClientRect();
                 ctx.beginPath();
-                ctx.rect(clientRectangle.left, clientRectangle.top, clientRectangle.right, clientRectangle.bottom);
+                ctx.rect(clientRectangle.left, clientRectangle.top, clientRectangle.right-10, clientRectangle.bottom-10);
                 ctx.stroke();
                 console.log(ctx);
                 resolve(c);
@@ -40,11 +63,12 @@ window.addEventListener("contextmenu",
         scr.then(c => {
             console.log("THEN");
             console.log(scr,c);
-            //document.getElementById("debugContent").innerHTML = "";
-            //document.getElementById("debugContent").appendChild(c);
+            document.getElementById("bugreport-image").innerHTML = "";
+            document.getElementById("bugreport-image").appendChild(c);
         });
         openPopup("bugreport");
         return false;
+        // https://stackoverflow.com/questions/13198131/how-to-save-an-html5-canvas-as-an-image-on-a-server
     }, false);
 function openPopup(popupId) {
     const popupModal = document.querySelector(`[data-bugreportermodal="${popupId}"]`);
@@ -62,6 +86,7 @@ document.body.innerHTML = document.body.innerHTML + `
             <input type="text"></input>
             <button>Save</button>
         </div>
+        <div id="bugreport-image"></image>
     </div>
     `;
 // CREATE CLOSE POPUP EVENTS
@@ -93,18 +118,19 @@ document.querySelector('.bugreportermodal__close').addEventListener('click', (ev
     }
 
     .bugreportermodal {
-    height: 365px;
-    width: 60%;
-    background-color: #fff;
-    position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%, -50%);
-    padding: 10px;
-    opacity: 0;
-    pointer-events: none;
-    transition: all 300ms ease-in-out;
-    z-index: 1011;
+        height: 365px;
+        width: 60%;
+        background-color: #fff;
+        position: absolute;
+        left: 50%;
+        top: 50%;
+        transform: translate(-50%, -50%);
+        padding: 10px;
+        opacity: 0;
+        pointer-events: none;
+        transition: all 300ms ease-in-out;
+        z-index: 1011;
+        overflow:scroll;
     }
     .bugreportermodal.is--visible {
         opacity: 1;
